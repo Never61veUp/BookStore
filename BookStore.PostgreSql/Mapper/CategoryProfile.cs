caea9136e11151dfa0fe23e713ed8dfa5de2875b
Алексей Яблонский <savageseed@yandex.ru>
2024-12-23 15:58:50 +0500

    feat(mapper profiles): mapper profile of categories

---
+++ BookStore.PostgreSql/Mapper/CategoryProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BookStore.Core.Model.Catalog;
+using BookStore.PostgreSql.Model;
+
+namespace BookStore.PostgreSql.Mapper;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<CategoryEntity, Category>()
+            .ConstructUsing(entity =>
+                Category.Create(
+                    entity.Id,
+                    entity.Name,
+                    entity.ParentCategory.Id
+                ).Value);
+        
+        CreateMap<Category, CategoryEntity>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.ParentCategory, opt => opt.Ignore()); // Обработка ParentCategory отдельно
+    }
+}
