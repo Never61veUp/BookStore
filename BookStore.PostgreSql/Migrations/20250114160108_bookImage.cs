1f100564bca72cd1576c5753430e60a8de858373
Алексей Яблонский <savageseed@yandex.ru>
2025-01-14 23:43:20 +0500

    build(postgresql): new migration
    bookImage

---
+++ BookStore.PostgreSql/Migrations/20250114160108_bookImage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class bookImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Books",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Books");
+        }
+    }
+}
