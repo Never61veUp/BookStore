db517ebe76726f05b8f3e064743738fd1e7922d8
Алексей Яблонский <savageseed@yandex.ru>
2025-01-18 17:31:32 +0500

    front(build):

--- bookstore.next/next.config.ts
+++ bookstore.next/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        domains: ["bookstore.s3.yandexcloud.net"],
+    },
 };
 
 export default nextConfig;
